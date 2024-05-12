d69b64b785342ceaf81acc9ce963d955fa62648e
AnshuByte <anshurauniyar2000@gmail.com>
2024-05-12 16:56:17 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Footer, Header, Whatgpt, Blog, Features, Possibility } from './containers'
+import { CTA, Brand, Navbar } from './components'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Whatgpt />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/whatGPT3/Whatgpt.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./whatgpt.css";
+const Whatgpt = () => {
+  return <div>whatgpt</div>;
+};
+
+export default Whatgpt;
